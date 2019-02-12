dd89541cc739818cfb43ff55f3e3c789f1abde8d
Dewi Jones <dewijones92@gmail.com>
2019-02-12 08:46:36 +0000

    Create install script from source.html

--- go.sh
+++ go.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-rm -rf bin/;
+BASE_DIR=$(pwd);
+function installcwm {
+	rm -rf bin/;
+	make install;
+	cp bin/chunkwm /usr/local/bin;
+	cp examples/chunkwmrc ~/.chunkwmrc
+	cp examples/com.koekeishiya.chunkwm.plist ~/Library/LaunchAgents;
+}
+function installchunc {
+	cd src/chunc;
+	make;
+	cp bin/chunkc /use/local/bin;
+}
+cd $BASE_DIR;
+installcwm;
+cd $BASE_DIR
+installchunc;
+cd $BASE_DIR;
+launchctl load -w ~/Library/LaunchAgents/com.koekeishiya.chunkwm.plist;
+
