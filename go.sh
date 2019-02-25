6e52153f752543655a447743c633cb4f80ff7e88
Dewi Jones <dewijones92@gmail.com>
2019-02-25 20:05:26 +0000

    Update stuff

--- go.sh
+++ go.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 launchctl unload  ~/Library/LaunchAgents/com.koekeishiya.chunkwm.plist;
 BASE_DIR=$(pwd);
-CUSTOM_CONFIG_DIR="custom_config"
+
 function installcwm {
 	rm -rf bin/
 	make install
 	cp bin/chunkwm /usr/local/bin
-	cp $CUSTOM_CONFIG_DIR/chunkwmrc ~/.chunkwmrc
+	cp examples/chunkwmrc ~/.chunkwmrc
 	cp examples/com.koekeishiya.chunkwm.plist ~/Library/LaunchAgents
 }
 function installchunc {
@@ -28,6 +28,6 @@ installchunc;
 cd $BASE_DIR;
 installplugins
 
-ln -f -s $BASE_DIR/$CUSTOM_CONFIG_DIR/chunkwmrc ~/.chunkwmrc
+ln -f -s $BASE_DIR/examples/chunkwmrc ~/.chunkwmrc
 launchctl load -F -w ~/Library/LaunchAgents/com.koekeishiya.chunkwm.plist;
 
